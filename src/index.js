c11b69b45b481ab4e375fb89865c3bdff5f78ff1
Matthew Fors <1824866+mattfors@users.noreply.github.com>
2024-12-05 11:21:37 -0500

    add date groupings

--- src/index.js
+++ src/index.js
@@ -1,19 +1,47 @@
 import './styles.css';
 import Handlebars from 'handlebars';
-import data from './assets/data.json';
+import data from './assets/grouped_output.json';
 import { format } from 'date-fns';
 import templateEvent from './templates/event.hbs';
 import '@shoelace-style/shoelace/dist/themes/dark.css';
 import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path'
 import '@shoelace-style/shoelace/dist/components/details/details';
+import '@shoelace-style/shoelace/dist/components/divider/divider';
+import '@shoelace-style/shoelace/dist/components/icon/icon';
+import '@shoelace-style/shoelace/dist/components/drawer/drawer';
 
-setBasePath('/node_modules/@shoelace-style/shoelace/dist');
+setBasePath('/shoelace');
 
 Handlebars.registerHelper('formatDate', function(dateString) {
-    return format(new Date(dateString), 'MMMM do, yyyy h:mm a');
+    return format(new Date(dateString), 'M/d EEEE');
+});
+
+Handlebars.registerHelper('formatTime', function(dateString) {
+    return format(new Date(dateString), 'h:mm a');
 });
 
 const template = Handlebars.compile(templateEvent);
 const html = template({ events: data });
+console.log(data)
 
-document.body.innerHTML = html;
+const mainContent = document.getElementById('main-content');
+mainContent.innerHTML = html;
+
+const container = document.querySelector('.details-group-example');
+const menuToggle = document.getElementById('menu-toggle');
+const drawer = document.querySelector('sl-drawer');
+
+menuToggle.addEventListener('click', () => {
+    drawer.open = !drawer.open;
+});
+
+container.addEventListener('sl-show', event => {
+    if (event.target.localName === 'sl-details') {
+        [...container.querySelectorAll('sl-details')].map(details => (details.open = event.target === details));
+    }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const drawer = document.querySelector('sl-drawer');
+    drawer.hidden = false;
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -38,10 +39,21 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
+            filename: 'index.html',
+            inject: 'body',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/qrcode.html',
+            filename: 'qrcode.html',
             inject: 'body',
         }),
         new MiniCssExtractPlugin({
             filename: '[name].css',
         }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'node_modules/@shoelace-style/shoelace/dist', to: 'shoelace' }
+            ]
+        })
     ],
 };
